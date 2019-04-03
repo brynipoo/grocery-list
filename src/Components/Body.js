bb898869a4e015f947faefd6b43390e0b76ebc01
Bryn Newell <bryn.newell@techtonic.com>
2019-04-03 10:56:13 -0600

    adding in mapping

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -7,20 +7,41 @@ export default class Body extends React.Component {
     super()
 
     this.state = {
-      itemTitles: []
+      itemTitles: [
+        'Popcorn',
+        'Ice cream'
+      ]
     }
+  }
+
+  addNewItem = (e) => {
+    e.preventDefault();
+    let newItem = document.querySelector('.new-item-input').value;
+    let newState = this.state.itemTitles;
+    newState.push(newItem)
+    this.setState({
+      itemTitles: newState,
+    })
+    document.querySelector('form').reset();
   }
 
   render() {
     return (
       <div className='body-container'>
         <h4>Grocery List</h4>
+        <form onSubmit={this.addNewItem}>
+          <label htmlFor='newItem'>Add to your grocery list</label>
+          <input type='text' name='newItem' className='new-item-input'></input>
+          <button onClick={this.addNewItem}>Add item</button>
+        </form>
         <header>
           <p>Purchased</p>
           <p>Item Title</p>
         </header>
-        <div class='list-body'>
-          <ListItem itemTitle='Popcorn' />
+        <div className='list-body'>
+        {this.state.itemTitles.map(item => {
+          return <ListItem itemTitle={item} key={item} />
+        }) }
         </div>
       </div>
     )
--- src/Components/ListItem.js
+++ src/Components/ListItem.js
@@ -2,13 +2,9 @@ import React from 'react';
 import './ListItem.css';
 
 export default class ListItem extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
   render() {
     return(
-      <div class='list-item-container'>
+      <div className='list-item-container'>
         <input type='checkbox' ></input>
         <p>{this.props.itemTitle}</p>
       </div>
